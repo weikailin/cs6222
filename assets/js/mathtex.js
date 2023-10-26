46fc07ad669ae501e22fbc52e3a7d67c15f7e5e8
Lin <twc7zv@virginia.edu>
2023-10-26 16:21:08 -0400

    test

--- assets/js/mathtex.js
+++ assets/js/mathtex.js
@@ -3,7 +3,32 @@
 
 // import katex from "katex";
 
-const macros = {};
+const macros = {
+      "\\set": "\\{#1\\}",
+      "\\bit": "\\set{0,1}",
+      "\\bits": "\\bit^\\ast",
+      "\\N": "\\mathbb{N}",
+      "\\eps": "\\epsilon",
+      "\\poly": "\\mathrm{poly}",
+      "\\Supp": "\\mathrm{Supp}",
+      "\\E": "\\mathop{\\mathbb{E}}",
+      "\\ceil": "{\\left\\lceil #1 \\right\\rceil}",
+      "\\floor": "{\\left\\lfloor #1 \\right\\rfloor}",
+      "\\cA": "\\mathcal{A}",
+      "\\cB": "\\mathcal{B}",
+      "\\cC": "\\mathcal{C}",
+      "\\cD": "\\mathcal{D}",
+      "\\cH": "\\mathcal{H}",
+      "\\cK": "\\mathcal{K}",
+      "\\cM": "\\mathcal{M}",
+      "\\cO": "\\mathcal{O}",
+      "\\cX": "\\mathcal{X}",
+      "\\cY": "\\mathcal{Y}",
+      "\\cZ": "\\mathcal{Z}",
+      "\\Gen": "\\mathsf{Gen}",
+      "\\pk": "\\mathit{pk}",
+      "\\sk": "\\mathit{sk}",
+    };
 
 let scripts = document.body.getElementsByTagName("script");
 scripts = Array.prototype.slice.call(scripts);
